42d5d734e3f510d42d0fc4254e87129b118af055
Muhammad Amien <muhammad.18017@mhs.unesa.ac.id>
2019-07-26 17:23:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
+/*
  // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:testestes.database.windows.net,1433; Database = test", "xperimental", "Bandar05");
@@ -35,11 +36,11 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
+*/
 // SQL Server Extension Sample Code:
-//$connectionInfo = array("UID" => "xperimental@testestes", "pwd" => "Bandar05", "Database" => "test", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-//$serverName = "tcp:testestes.database.windows.net,1433";
-//$conn = sqlsrv_connect($serverName, $connectionInfo);
+$connectionInfo = array("UID" => "xperimental@testestes", "pwd" => "Bandar05", "Database" => "test", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testestes.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	/*
     $host = "localhost";
