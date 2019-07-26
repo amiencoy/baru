9fa0596aa4d4cabfb0e45b220cd503e9b21a061c
Muhammad Amien <muhammad.18017@mhs.unesa.ac.id>
2019-07-26 16:48:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,9 +37,9 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "xperimental@testestes", "pwd" => "Bandar05", "Database" => "test", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:testestes.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+//$connectionInfo = array("UID" => "xperimental@testestes", "pwd" => "Bandar05", "Database" => "test", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+//$serverName = "tcp:testestes.database.windows.net,1433";
+//$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	/*
     $host = "localhost";
